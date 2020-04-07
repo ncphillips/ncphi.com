d86c8bd5516733e0985bde0e5f04498e352c8c91
Nolan Phillips <ncphillips.19@gmail.com>
2020-04-07 18:46:41 -0300

    setup getStaticProps and form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,104 @@
-import React from "react";
+import React from 'react';
+import {
+  useGithubErrorListener,
+  useGithubJsonForm,
+  GithubOptions,
+} from 'react-tinacms-github';
+import { GetStaticProps } from 'next';
+import {
+  getJsonFile,
+  GithubError,
+  SourceProviderConnection,
+} from 'next-tinacms-github';
+import { EditLink } from '../components/EditLink';
 
-export default function HomePage() {
-  return <div>Hello World</div>;
+interface Props {
+  home: any;
+  sourceProvider: GithubOptions;
+  editMode: boolean;
 }
+
+export default function HomePage(props: Props) {
+  const [, form]: any = useGithubJsonForm(
+    props.home,
+    {
+      fields: [{ name: 'title', component: 'text' }],
+    },
+    props.sourceProvider
+  );
+
+  useGithubErrorListener(form);
+
+  return (
+    <div>
+      <EditLink editMode={props.editMode} />
+      Hello World
+    </div>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async function ({
+  preview,
+  previewData,
+}) {
+  const accessToken = previewData?.github_access_token || 'DEFAULT';
+  const sourceProviderConnection = {
+    forkFullName:
+      previewData?.fork_full_name || 'https://github.com/ncphillips/ncphi.com',
+    headBranch: previewData?.head_branch || 'master',
+  };
+  let previewError: GithubError = null;
+  let homeData = {};
+  try {
+    homeData = await getJsonFile(
+      'src/content/home.json',
+      sourceProviderConnection,
+      accessToken
+    );
+  } catch (e) {
+    if (e instanceof GithubError) {
+      previewError = { ...e }; //workaround since we cant return error as JSON
+    } else {
+      throw e;
+    }
+  }
+
+  return {
+    props: {
+      home: homeData,
+      previewError: previewError,
+      sourceProviderConnection,
+      editMode: !!preview,
+    },
+  };
+};
+
+interface Response {
+  accessToken: string | null;
+  sourceProviderConnection: SourceProviderConnection | null;
+}
+
+interface PreviewData {
+  fork_full_name: string;
+  head_branch: string;
+  github_access_token: string;
+}
+
+export const getGithubDataFromPreviewProps = (
+  previewData?: PreviewData
+): Response => {
+  if (!previewData) {
+    return {
+      sourceProviderConnection: null,
+      accessToken: null,
+    };
+  }
+
+  return {
+    accessToken: previewData.github_access_token,
+    sourceProviderConnection: {
+      forkFullName: previewData.fork_full_name,
+      headBranch: previewData.head_branch || 'master',
+    },
+  };
+};
