b203ed77837ba367b8d5109dd6714e27f1eb0217
Nolan Phillips <ncphillips.19@gmail.com>
2020-04-08 17:34:59 -0300

    correct github_access_token

--- src/pages/api/proxy-github.ts
+++ src/pages/api/proxy-github.ts
@@ -1,5 +1,5 @@
 import { createProxy } from 'next-tinacms-github';
 
-const GITHUB_ACCESS_TOKEN_COOKIE_KEY = '';
+const GITHUB_ACCESS_TOKEN_COOKIE_KEY = 'github_access_token';
 
 export default createProxy(GITHUB_ACCESS_TOKEN_COOKIE_KEY);
