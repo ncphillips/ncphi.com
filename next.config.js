46fe8f25617622f80316c091491787f2a5a1c5fe
Nolan Phillips <ncphillips.19@gmail.com>
2020-04-08 17:39:11 -0300

    chore: load gh client id/secret into env

--- next.config.js
+++ next.config.js
@@ -1,35 +1,39 @@
-const fs = require("fs");
+const fs = require('fs');
 module.exports = {
-  webpack: function(config) {
+  env: {
+    GITHUB_CLIENT_ID: process.env.NCPHI_COM_GH_ID,
+    GITHUB_CLIENT_SECRET: process.env.NCPHI_COM_GH_SECRET,
+  },
+  webpack: function (config) {
     config.module.rules.push({
       test: /\.md$/,
-      use: "raw-loader"
+      use: 'raw-loader',
     });
     return config;
   },
-  exportPathMap: async function(
+  exportPathMap: async function (
     defaultPathMap,
     { dev, dir, outDir, distDir, buildId }
   ) {
     const pathMap = {
-      "/": { page: "/" }
+      '/': { page: '/' },
     };
 
     /**
      * Generate Path Maps for all posts
      */
-    const posts = fs.readdirSync("./src/content/blog");
+    const posts = fs.readdirSync('./src/content/blog');
 
-    posts.forEach(filename => {
-      const slug = filename.replace(".md", "");
+    posts.forEach((filename) => {
+      const slug = filename.replace('.md', '');
       pathMap[`/blog/${slug}`] = {
-        page: "/blog/[slug]",
+        page: '/blog/[slug]',
         query: {
-          slug
-        }
+          slug,
+        },
       };
     });
 
     return pathMap;
-  }
+  },
 };
