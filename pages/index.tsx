4ce1fccf777f1a2bdd53fda3f9452a3f06b335a7
Nolan Phillips <ncphillips.19@gmail.com>
2020-05-05 16:51:24 -0300

    load files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
-import Head from 'next/head'
+import Head from "next/head"
+import { GetStaticProps } from "next"
+import { getGithubPreviewProps, parseJson } from "next-tinacms-github"
 
-export default function Home() {
+export default function Home({ file }) {
+  const data = file.data
   return (
     <div className="container">
       <Head>
@@ -54,7 +57,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
         </a>
       </footer>
@@ -206,4 +209,28 @@ export default function Home() {
       `}</style>
     </div>
   )
+}
+
+export const getStaticProps: GetStaticProps = async function ({
+  preview,
+  previewData,
+}) {
+  if (preview) {
+    return getGithubPreviewProps({
+      ...previewData,
+      fileRelativePath: "content/home.json",
+      parse: parseJson,
+    })
+  }
+  return {
+    props: {
+      sourceProvider: null,
+      error: null,
+      preview: false,
+      file: {
+        fileRelativePath: "content/home.json",
+        data: (await import("../content/home.json")).default,
+      },
+    },
+  }
 }
