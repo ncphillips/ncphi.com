6bbfa3ff4685797cf66c2197297847a07cfaa82a
Nolan Phillips <ncphillips.19@gmail.com>
2020-05-05 17:38:43 -0300

    connect to repo

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+const { aliasTinaDev } = require("@tinacms/webpack-helpers")
+
+require("dotenv").config()
 
 module.exports = {
   env: {
@@ -6,5 +8,15 @@ module.exports = {
     REPO_FULL_NAME: process.env.REPO_FULL_NAME,
     BASE_BRANCH: process.env.BASE_BRANCH,
   },
-  // ...
-};
+  webpack(config) {
+    if (process.env.TINA) {
+      let watch
+      if (process.env.TINA_WATCH) {
+        watch = process.env.TINA_WATCH.split(",")
+      }
+      aliasTinaDev(config, process.env.TINA, watch)
+    }
+
+    return config
+  },
+}
