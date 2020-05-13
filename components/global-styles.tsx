7bbea16006ad7ff225d530acfb386bb51fd2bdfc
Nolan Phillips <ncphillips.19@gmail.com>
2020-05-12 21:53:29 -0300

    extract footer and global styles components

---
+++ components/global-styles.tsx
@@ -0,0 +1,15 @@
+export const GlobalStyles = () => (
+  <style jsx global>{`
+    html,
+    body {
+      padding: 0;
+      margin: 0;
+      font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+        Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    }
+
+    * {
+      box-sizing: border-box;
+    }
+  `}</style>
+)
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,7 @@ import {
   InlineTextareaField,
 } from "react-tinacms-inline"
 import { HOME_BLOCKS } from "../blocks/home"
+import { GlobalStyles } from "../components/global-styles"
 
 export default function Home({ file, preview }) {
   const [, form] = useGithubJsonForm(file, {})
@@ -40,17 +41,6 @@ export default function Home({ file, preview }) {
         </InlineForm>
       </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
       <style jsx>{`
         .container {
           min-height: 100vh;
@@ -66,25 +56,6 @@ export default function Home({ file, preview }) {
           flex: 1;
           display: flex;
           flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
           justify-content: center;
           align-items: center;
         }
@@ -140,36 +111,6 @@ export default function Home({ file, preview }) {
           margin-top: 3rem;
         }
 
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
         .logo {
           height: 1em;
         }
@@ -181,21 +122,7 @@ export default function Home({ file, preview }) {
           }
         }
       `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+      <GlobalStyles />
     </div>
   )
 }
