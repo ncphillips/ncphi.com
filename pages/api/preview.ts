e8c8616aae60d7ff5752ba14a983f264e40a3db8
ncphi <nolan@ncphi.com>
2021-12-17 11:41:27 -0400

    Delete website code

--- pages/api/preview.ts
+++
@@ -1,8 +0,0 @@
-import { previewHandler } from "next-tinacms-github"
-
-const preview = previewHandler("hello-world")
-
-export default (req, res) => {
-  console.log(preview)
-  return preview(req, res)
-}
